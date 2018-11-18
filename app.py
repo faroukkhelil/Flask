5526cd10d08d4690bb93f50c22f6866ef1c50ee9
Farouk KHELIL <farouk.khelil@me.com>
2018-11-18 16:59:21 +0100

    Fix port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from mocks import Post
 
@@ -39,4 +40,5 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
